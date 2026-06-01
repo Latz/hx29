76c706189c846ea7ce0cfcf66a2019e343f3fe2c
Latz <latz@elektroelch.de>
2026-06-01 20:01:24 +0200

    Add --help hint to man ls page

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '813e176fb4411976e54c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '460f19448647cdc2a27c');
