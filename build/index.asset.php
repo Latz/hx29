def9ab0c1430d3216eccb20cb10c17c29c5391bc
Latz <latz@elektroelch.de>
2026-06-01 03:17:12 +0200

    Baud-stutter: double freeze length to 200-500ms

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '994e5ab4b386adb93aca');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '1460fbacc8d396a13651');
