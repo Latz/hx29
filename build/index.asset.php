9d2535caddf033b5557d591ad90d8dff89e344bf
Latz <latz@elektroelch.de>
2026-08-03 15:26:44 +0200

    Rebuild compiled bundle

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8605345635bdb2c4f55e');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3544b13ca1183a5fab82');
